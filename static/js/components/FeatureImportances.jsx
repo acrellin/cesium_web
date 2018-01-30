f4155720cb93e3b8f792600fd2d89bbc5bb90a0a
Ari Crellin-Quick <a.crellinquick@gmail.com>
2018-01-30 10:16:53 -0800

    Set feature importances chart axis minimum to zero

--- static/js/components/FeatureImportances.jsx
+++ static/js/components/FeatureImportances.jsx
@@ -5,8 +5,7 @@ import { HorizontalBar } from 'react-chartjs-2';
 const FeatureImportancesBarchart = props => {
   const sorted_features = Object.keys(props.data).sort(
     (a, b) => props.data[b] - props.data[a]).slice(0, 15);
-  const values = sorted_features.map(
-    feature => props.data[feature].toFixed(3));
+  const values = sorted_features.map(feature => props.data[feature]);
   const data = {
     labels: sorted_features,
     datasets: [
@@ -18,10 +17,19 @@ const FeatureImportancesBarchart = props => {
       }
     ]
   };
+  const options = {
+    scales: {
+      xAxes: [{
+        ticks: {
+          min: 0.0
+        }
+      }]
+    }
+  };
 
   return (
     <div style={{ height: 300, width: 600 }}>
-      <HorizontalBar data={data} />
+      <HorizontalBar data={data} options={options} />
     </div>
   );
 };
