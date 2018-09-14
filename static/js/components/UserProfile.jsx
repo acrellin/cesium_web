e85ae23852be9cd1b9e2ae5ebcc6369a456ac795
Ari Crellin-Quick <a.crellinquick@gmail.com>
2018-09-14 14:29:51 -0700

    De-lint JS sources

--- static/js/components/UserProfile.jsx
+++ static/js/components/UserProfile.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 let UserProfile = props => (
   <div style={props.style}>{props.profile.username}</div>
 );
 UserProfile.propTypes = {
-  style: React.PropTypes.object.isRequired,
-  profile: React.PropTypes.object.isRequired
+  style: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => (
